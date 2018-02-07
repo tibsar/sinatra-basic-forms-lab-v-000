01cb46f45b99c5dfeadb72a568970706bff088e0
Sara Tibbetts <sxt4866@rit.edu>
2018-02-07 15:16:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,6 +12,7 @@ class App < Sinatra::Base
   
   post '/display_puppy' do 
     binding.pry
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end 
 end
